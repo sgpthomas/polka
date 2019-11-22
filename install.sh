ab0dd73c7adff81d22239c2d207e21679eccdfc8
Samuel Thomas <sgpthomas@gmail.com>
2019-11-22 00:55:37 -0500

    fix typos

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 function download() {
   rm -rf oh-my-zsh
   git clone https://github.com/sgpthomas/oh-my-zsh --recursive
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git oh-mh-zsh/plugins
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/plugins/zsh-syntax-highlighting
   cp -r oh-my-zsh ~/.oh-my-zsh
 }
 
