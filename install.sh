dc01951674bbcf808333aa7bf05453e551ec9e38
Samuel Thomas <sgpthomas@gmail.com>
2019-11-22 00:36:47 -0500

    updated install

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 function download() {
-  git clone https://github.com/sgpthomas/oh-my-zsh
+  rm -rf oh-my-zsh
+  git clone https://github.com/sgpthomas/oh-my-zsh --recursive
   cp -r oh-my-zsh ~/.oh-my-zsh
 }
 
@@ -9,6 +10,8 @@ function install() {
   ln zshrc ~/.zshrc  
   touch ~/.zshenv
   touch ~/.zshprojects
+  touch ~/.zshalias
+  touch ~/.zshlocal
 }
 
 download
